2a3dc8d81bd613f914bc19c218f54cb066677ca3
Robyn Felicity Heather Veitch <robynfhveitch@gnail.com>
2026-08-24 21:25:43 +0100

    ci: removed test block

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-    css: true,
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'html'],
-      exclude: ['src/test/**'],
-    },
-  },
+//   test: {
+//     environment: 'jsdom',
+//     setupFiles: './src/test/setup.ts',
+//     css: true,
+//     coverage: {
+//       provider: 'v8',
+//       reporter: ['text', 'html'],
+//       exclude: ['src/test/**'],
+//     },
+//   },
 })
