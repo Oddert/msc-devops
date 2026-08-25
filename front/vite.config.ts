1284049cb615133dbbb17e257bec57d1da008e9d
Robyn Felicity Heather Veitch <robynfhveitch@gnail.com>
2026-08-25 21:48:27 +0100

    test: added missing global flag

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -5,6 +5,8 @@ export default defineConfig({
   plugins: [react()],
   base: './',
   test: {
+    include: ['**/*.test.tsx', '**/*.test.ts'],
+    globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
     css: true,
